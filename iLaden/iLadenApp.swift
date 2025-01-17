a987830525aae612ac4b522f8008d88c31f4b3d0
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-01-17 06:04:31 +0200

    Initial Commit

---
+++ iLaden/iLadenApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLadenApp.swift
+//  iLaden
+//
+//  Created by Mohamed Sayed on 17.01.25.
+//
+
+import SwiftUI
+
+@main
+struct iLadenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
