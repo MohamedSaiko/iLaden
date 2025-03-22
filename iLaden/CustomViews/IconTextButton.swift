14819cc8ce599d370571e0ae3099785263dc17c1
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-22 15:22:39 +0100

    Add IconTextButton

--- iLaden/CustomViews/IconTextButton.swift
+++ iLaden/CustomViews/IconTextButton.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct IconTextButton: View {
+    private let icon: String
+    private let text: String
+    private let textSize: CGFloat
+    private let minHeight: CGFloat
+    private let maxWidth: CGFloat
+    private let alignment: Alignment
+    private let action: () -> Void
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    public init(icon: String = .isGoogleIcon(), text: String = "", textSize: CGFloat = 20, minHeight: CGFloat = 20, maxWidth: CGFloat = .infinity, alignment: Alignment = .center, action: @escaping () -> Void = {}) {
+        self.icon = icon
+        self.text = text
+        self.textSize = textSize
+        self.minHeight = minHeight
+        self.maxWidth = maxWidth
+        self.alignment = alignment
+        self.action = action
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            action()
+        } label: {
+            HStack() {
+                Image(icon)
+                    .padding()
+                
+                Spacer()
+                
+                Text("Continue With Google")
+                    .font(.isPrimary(withSize: textSize))
+                    .padding()
+                
+                Spacer()
+            }
+        }
+        .frame(maxWidth: maxWidth ,minHeight: minHeight, alignment: alignment)
+        .background(Color.isSecondaryBackgroundColor())
+        .foregroundColor(colorScheme == .dark ? .white : .black)
+        .clipShape(Capsule())
+        .padding()
     }
 }
 
