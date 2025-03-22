9aacca4a78daf4dc97b2ac1d8de7c81b08cbb47c
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-22 15:20:16 +0100

    Add StringExtension

---
+++ iLaden/Extensions/StringExtension.swift
@@ -0,0 +1,12 @@
+//
+//  StringExtension.swift
+//  iLaden
+//
+//  Created by Mohamed Sayed on 22.03.25.
+//
+
+extension String {
+    static func isGoogleIcon() -> String {
+        return "GoogleIcon"
+    }
+}
