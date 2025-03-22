4956867a603567fe7e7062474be0926d792b6814
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-22 15:17:23 +0100

    update the project

---
+++ iLaden/CustomViews/IconTextButton.swift
@@ -0,0 +1,18 @@
+//
+//  IconTextButton.swift
+//  iLaden
+//
+//  Created by Mohamed Sayed on 22.03.25.
+//
+
+import SwiftUI
+
+struct IconTextButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconTextButton()
+}
---
+++ iLaden/Fonts/FontExtension.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  iLaden
+//
+//  Created by Mohamed Sayed on 22.03.25.
+//
+
