2cd70337f4e37bb630cadf81d4af41c08de9bba1
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-24 13:04:47 +0100

    Add GoogleSignInManager

--- iLaden/SignIn/GoogleSignIn/GoogleSignInManager.swift
+++ iLaden/SignIn/GoogleSignIn/GoogleSignInManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import GoogleSignIn
+
+final class GoogleSignInManager: AnyGoogleSignInManager {
+    func signIn() {
+        GIDSignIn.sharedInstance.signIn(withPresenting: RootView().getRootViewController()) { signInResult, error in
+            guard error == nil, let result = signInResult else {
+                print("Error Signing In With Google: \(error?.localizedDescription ?? "No error description")")
+                return
+            }
+            
+            guard let idToken = result.user.idToken?.tokenString else { return }
+            print("JWT ID Token: \(idToken)")
+        }
+    }
+}
