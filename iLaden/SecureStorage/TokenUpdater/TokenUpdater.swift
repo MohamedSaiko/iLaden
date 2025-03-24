62069423a51da5b0768669b910edf0d21567d183
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-24 05:25:40 +0100

    Add TokenUpdater

--- iLaden/SecureStorage/TokenUpdater/TokenUpdater.swift
+++ iLaden/SecureStorage/TokenUpdater/TokenUpdater.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+final class TokenUpdater: AnyTokenUpdater {
+    private let secureStorageManager: AnySecureStorageManager
+    
+    init(secureStorageManager: AnySecureStorageManager = KeyChainManager()) {
+        self.secureStorageManager = secureStorageManager
+    }
+    
+    func updateToken(token: String, forKey key: String) throws {
+        try secureStorageManager.update(secret: token, forKey: key)
+    }
+}
