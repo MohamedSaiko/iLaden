8596b8cd4f9cc596d9e6b9bf5e9dfebd19e8e67c
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-24 13:03:11 +0100

    Add GoogleSignInManager to LoginViewModel

--- iLaden/Login/LoginViewModel.swift
+++ iLaden/Login/LoginViewModel.swift
@@ -12,14 +12,16 @@ final class LoginViewModel: ObservableObject {
     private let tokenGetter: AnyTokenGetter
     private let tokenUpdater: AnyTokenUpdater
     private let tokenRemover: AnyTokenRemover
+    private let googleSignInManager: AnyGoogleSignInManager
     
     @Published private(set) var upToDateToken: String = ""
     
-    init(tokenSetter: AnyTokenSetter = TokenSetter(), tokenGetter: AnyTokenGetter = TokenGetter(), tokenUpdater: AnyTokenUpdater = TokenUpdater(), tokenRemover: AnyTokenRemover = TokenRemover()) {
+    init(tokenSetter: AnyTokenSetter = TokenSetter(), tokenGetter: AnyTokenGetter = TokenGetter(), tokenUpdater: AnyTokenUpdater = TokenUpdater(), tokenRemover: AnyTokenRemover = TokenRemover(), googleSignInManager: AnyGoogleSignInManager = GoogleSignInManager()) {
         self.tokenSetter = tokenSetter
         self.tokenGetter = tokenGetter
         self.tokenUpdater = tokenUpdater
         self.tokenRemover = tokenRemover
+        self.googleSignInManager = googleSignInManager
     }
     
     func setToken(token: String) {
@@ -51,5 +53,9 @@ final class LoginViewModel: ObservableObject {
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    func signInWithGoogle() {
+        googleSignInManager.signIn()
     }
 }
