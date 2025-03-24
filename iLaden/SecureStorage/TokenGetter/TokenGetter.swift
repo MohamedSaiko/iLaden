a9ec7c5bedbf2c345dec77a103fd7a2dedc1de4e
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-24 05:23:35 +0100

    Add TokenGetter

--- iLaden/SecureStorage/TokenGetter/TokenGetter.swift
+++ iLaden/SecureStorage/TokenGetter/TokenGetter.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+final class TokenGetter: AnyTokenGetter {
+    private let secureStorageManager: AnySecureStorageManager
+    
+    init(secureStorageManager: AnySecureStorageManager = KeyChainManager()) {
+        self.secureStorageManager = secureStorageManager
+    }
+    
+    func getToken(forKey key: String) -> String {
+        guard let token = secureStorageManager.get(forKey: key) else { return ""}
+        return token
+    }
+}
