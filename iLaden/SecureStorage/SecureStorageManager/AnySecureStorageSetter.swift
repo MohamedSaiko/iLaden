545dda7166a630a770969ed5724823523e9ac92b
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-27 09:58:37 +0100

    Add AnySecureStorageSetter

--- iLaden/SecureStorage/SecureStorageManager/AnySecureStorageSetter.swift
+++ iLaden/SecureStorage/SecureStorageManager/AnySecureStorageSetter.swift
@@ -1,7 +1,10 @@
 //
-//  Untitled.swift
+//  AnySecureStorageSetter.swift
 //  iLaden
 //
 //  Created by Mohamed Sayed on 27.03.25.
 //
 
+protocol AnySecureStorageSetter {
+    func setSecret(secret: String, forKey key: String) throws
+}
