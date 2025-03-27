0faa797e80f021bda736e137ef677d55a43fc90d
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-27 10:00:31 +0100

    Add Add KeyChainError

--- iLaden/SecureStorage/SecureStorageManager/KeychainError.swift
+++ iLaden/SecureStorage/SecureStorageManager/KeychainError.swift
@@ -1,7 +1,13 @@
 //
-//  Untitled.swift
+//  KeychainError.swift
 //  iLaden
 //
 //  Created by Mohamed Sayed on 27.03.25.
 //
 
+import Foundation
+
+enum KeychainError: Error {
+    case duplicateEntry
+    case unknown(OSStatus)
+}
