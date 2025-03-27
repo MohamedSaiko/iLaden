893e4a434a1c3f092a5497325d7de236e3b43e74
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-27 09:06:45 +0100

    Add getGoogleUserSignInState function to LoginViewModel

--- iLaden/Login/LoginViewModel.swift
+++ iLaden/Login/LoginViewModel.swift
@@ -14,15 +14,17 @@ final class LoginViewModel: ObservableObject {
     private let tokenUpdater: AnyTokenUpdater
     private let tokenRemover: AnyTokenRemover
     private let googleSignInManager: AnyGoogleSignInManager
+    private let googleUserSignInStateManager: AnyGoogleUserSignInStateManager
     
     @Published private(set) var upToDateToken: String = ""
     
-    init(tokenSetter: AnyTokenSetter = TokenSetter(), tokenGetter: AnyTokenGetter = TokenGetter(), tokenUpdater: AnyTokenUpdater = TokenUpdater(), tokenRemover: AnyTokenRemover = TokenRemover(), googleSignInManager: AnyGoogleSignInManager = GoogleSignInManager()) {
+    init(tokenSetter: AnyTokenSetter = TokenSetter(), tokenGetter: AnyTokenGetter = TokenGetter(), tokenUpdater: AnyTokenUpdater = TokenUpdater(), tokenRemover: AnyTokenRemover = TokenRemover(), googleSignInManager: AnyGoogleSignInManager = GoogleSignInManager(), googleUserSignInStateManager: AnyGoogleUserSignInStateManager = GoogleUserSignInStateManager()) {
         self.tokenSetter = tokenSetter
         self.tokenGetter = tokenGetter
         self.tokenUpdater = tokenUpdater
         self.tokenRemover = tokenRemover
         self.googleSignInManager = googleSignInManager
+        self.googleUserSignInStateManager = googleUserSignInStateManager
     }
     
     func setToken(token: String) {
@@ -60,8 +62,24 @@ final class LoginViewModel: ObservableObject {
         googleSignInManager.signIn() {(result: Result<GIDSignInResult, Error>) in
             switch result {
             case .success(let googleSignInResult):
+                guard let idToken = googleSignInResult.user.idToken?.tokenString else { return }
+                print("JWT ID Token: \(idToken)")
                 DispatchQueue.main.async {
                     completion(.success(googleSignInResult))
+                }
+                
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func getGoogleUserSignInState(completion: @escaping (Result<GIDGoogleUser, Error>) -> Void) {
+        googleUserSignInStateManager.getUserSignInState() {(result: Result<GIDGoogleUser, Error>) in
+            switch result {
+            case .success(let googleCurrentUser):
+                DispatchQueue.main.async {
+                    completion(.success(googleCurrentUser))
                 }
                 
             case .failure(let error):
