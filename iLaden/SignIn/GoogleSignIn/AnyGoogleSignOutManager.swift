b05b5d9045629dbed3d422bf117a0d17e4153ab8
MohamedSayed <Mohamedsaiko9@gmail.com>
2025-03-27 09:03:52 +0100

    Add AnyGoogleSignOutManager

---
+++ iLaden/SignIn/GoogleSignIn/AnyGoogleSignOutManager.swift
@@ -0,0 +1,12 @@
+//
+//  AnyGoogleSignOutManager.swift
+//  iLaden
+//
+//  Created by Mohamed Sayed on 27.03.25.
+//
+
+import GoogleSignIn
+
+protocol AnyGoogleSignOutManager {
+    func signOut()
+}
